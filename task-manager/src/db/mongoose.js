1e17c3ed54edfc443e06cb02c6597767e44a096c
huynhviha1703@gmail.com <huynhviha1703@gmail.com>
2022-09-22 11:23:25 +0700

    get user and task

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,20 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://nemo-admin:Anhduy2507@cluster0.7qy3g.mongodb.net/task-manager-api?retryWrites=true&w=majority",
+  "mongodb+srv://nemo-admin:pw@cluster0.7qy3g.mongodb.net/task-manager-api?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
   }
 );
 
-// const newTask = new Task({ description: "                        clean house" })
-
-// newTask.save()
-//   .then(returnedTask => console.log(returnedTask))
-//   .catch(err => console.log(err))
-
-// const me = new User({ name: "Nemo", age: 25, email: "huUYhvihA1703@gmail.com", password: "  123456word   " });
-
-// me.save()
-//   .then((returnedUser) => console.log(returnedUser))
-//   .catch((err) => console.log(err));
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 require("./db/mongoose")
 const User = require("./models/user")
 const Task = require("./models/task")
+const { response } = require("express")
 
 const app = express()
 app.use(express.json())
@@ -16,12 +17,41 @@ app.post("/users", (req, res) => {
         .catch(err => res.status(400).json(err))
 })
 
+app.get("/users", (req, res) => {
+    User.find({})
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json(err))
+})
+
+app.get("/users/:id", (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) res.status(404).json({ message: "No user found" })
+            res.json(user)
+        })
+        .catch(err => res.status(500).json(err))
+})
+
 app.post("/tasks", (req, res) => {
     const { body } = req
     const newTask = new Task(body)
     newTask.save().then(result => res
         .json(result))
         .catch(err => res.status(400).json(err))
+})
+
+app.get("/tasks", (req, res) => {
+    Task.find({})
+        .then(tasks => res.json(tasks))
+        .catch(err => res.status(500).json(err))
+})
+
+app.get("/tasks/:id", (req, res) => {
+    Task.findById(req.params.id)
+        .then(task => {
+            if (!task) res.status(404).json({ message: "No task found" })
+            res.json(task)
+        }).catch(err => res.status(500).json(err))
 })
 
 app.listen(PORT, () => {
