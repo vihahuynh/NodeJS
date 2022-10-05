a09be8cc80d303ccf2f7f39d0f1e9c88cf3ea904
huynhviha1703@gmail.com <huynhviha1703@gmail.com>
2022-10-05 17:58:35 +0700

    working with timestamp

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -14,7 +14,9 @@ const Task = mongoose.model('Task', {
     owner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
-})
+    },
+}, { timestamps: true })
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -52,7 +52,7 @@ const userSchema = new mongoose.Schema({
   //   type: mongoose.Schema.Types.ObjectId,
   //   ref: 'Task'
   // }]
-});
+}, { timestamps: true });
 
 // virtual property
 userSchema.virtual('tasks', {
